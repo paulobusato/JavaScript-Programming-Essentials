e62343d5ffab6463398f2a3911bc880cc428fcd1
Paulo Busato <paulo.busato2@gmail.com>
2026-02-18 22:00:24 -0300

    product_survey task

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,6 +8,7 @@ function submitFeedback() {
     const designation = document.getElementById("designation").value;
     const productType = document.getElementById("productType").value;
     const feedback = document.getElementById("feedbackText").value;
+    const experience = document.getElementById("userExperienceText").value;
 
     alert(`Thank you for your valuable feedback`);
 
@@ -18,6 +19,7 @@ function submitFeedback() {
     document.getElementById("userDesignation").innerHTML = designation;
     document.getElementById("userProductChoice").innerHTML = productType;
     document.getElementById("userFeedback").innerHTML = feedback;
+    document.getElementById("userExperience").innerHTML = experience;
 
     document.getElementById("userInfo").style.display = "block";
 }
