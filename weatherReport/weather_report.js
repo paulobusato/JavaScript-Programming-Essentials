26d8a1cc4455e8c9c69170c0e1f20798f45a92a1
Paulo Henrique Busato <paulo.busato2@gmail.com>
2026-05-01 08:40:39 -0300

    added functionality to fetch weather by latitude and longitude

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -22,4 +22,26 @@ function showweatherDetails(event) {
         })
 }
 
-document.getElementById("weatherForm").addEventListener("click", showweatherDetails);
+document.getElementById("fetchByCityForm").addEventListener("click", showweatherDetails);
+
+function fetchByLatLongForm(event) {
+    event.preventDefault()
+
+    const latitude = document.getElementById("latitude").value;
+    const longitude = document.getElementById("longitude").value;
+    const apiKey = "YOUR_API_KEY";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById("weatherInfo");
+           weatherInfo.innerHTML = `
+               <h2>Weather in ${data.name}</h2>
+               <p>Temperature: ${data.main.temp} &#8451;</p>
+               <p>Weather: ${data.weather[0].description}</p>
+           `;
+        });
+}
+
+document.getElementById("fetchByLatLongForm").addEventListener("click", fetchByLatLongForm);
